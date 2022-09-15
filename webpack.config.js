09fcbd8a8aa9679e21f6372d9563e223cf02f512
DESKTOP-75E37KC\erik.ferrari <erik.ferrari@acsoftware.it>
2022-09-15 17:37:46 +0200

    Fix error on bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,48 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
-  externals: [nodeExternals()],
-  output: {
-    path: path.join(__dirname, "./dist/"),
-    filename: "hyt-u.js",
-    library: ["hyt-ui"],
-    libraryTarget: "umd",
-    publicPath: "/dist/",
+  externals: {
+    react: "react",
+    reactDOM: "react-dom",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"],
+    alias: {
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+    },
   },
   plugins: [new CleanWebpackPlugin()],
+  output: {
+    path: path.join(__dirname, "./dist/"),
+    filename: "hyt-ui.js",
+    library: {
+      name: "hyt-ui",
+      type: "umd",
+    },
+    publicPath: "/dist/",
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
       },
       {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, "./src"),
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
